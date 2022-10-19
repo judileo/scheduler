a689fb231a135edce8fea50cf0212a6034dfca39
ArianaTrinidad <quirino.ariana1@gmail.com>
2022-10-19 18:20:44 -0300

    I couldn't throw an exception yet, but I wanted to move forward with other tasks

---
+++ scheduler.api/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace scheduler.api.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails
+                {
+                    Title = $"Oops! Something went wrong, please try again. The error status code is {httpContext.Response.StatusCode}"
+                }));
+
+                Console.WriteLine(ex.Message + ex.HelpLink);
+
+
+            }
+        }
+    }
+}
